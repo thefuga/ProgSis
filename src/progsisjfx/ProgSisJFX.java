f6890d89213cc4a9b472f0ddc3732dba09542964
Erick Costa Fuga <erick_fuga@hotmail.com>
2017-06-06 21:55:55 -0300

    Projeto JFX adicionado

---
+++ src/progsisjfx/ProgSisJFX.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author erickfuga
+ */
+package progsisjfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class ProgSisJFX extends Application {
+    
+    /**
+     * 
+     * @param primaryStage 
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/progsisjfx/view/RootLayoutController.java
@@ -0,0 +1,11 @@
+/**
+ * TODO
+ *
+ * @author erickfuga
+ */
+package progsisjfx.view;
+
+
+public class RootLayoutController {
+
+}
